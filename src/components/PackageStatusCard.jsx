52312246f0c9e8b62e72c06c65af9d7a7b084c3f
jay mbans <jordan@mbanefo.com>
2024-02-20 22:17:52 -0500

    added function from interview

--- src/components/PackageStatusCard.jsx
+++ src/components/PackageStatusCard.jsx
@@ -34,16 +34,98 @@ function PackageStatusCard() {
   const [countError, setCountError] = useState(false)
   const [orderError, setOrderError] = useState(false)
   const [success, setSuccess] = useState(false)
+  const [packageStr, setPackageStr] = useState("")
 
   //functions
+  const packBoxes = (boxes) => {
+    if (!boxes.length) return false
+
+    //add type of boxes to hash
+    const hash = {
+      "W": "w",
+      "S": "s",
+      "C": "c",
+      "A": "a"
+    }
+
+    //store the bottom in a stack
+    const stack = []
+
+    for (let piece of boxes) {
+      //check if closed on open
+      if (piece in hash) {
+        const mostRecentBottom = stack.pop()
+        if (hash[piece] !== mostRecentBottom) return [false, `Error - Order Mismatch: Cannot close box type ${piece} with type ${mostRecentBottom}`]
+      } else {
+        stack.push(piece)
+      }
+    }
+
+    return [stack.length === 0, ""]
+  }
+
   const submitRecord = (e) => {
+    setErrors("")
+
     e.preventDefault()
+
+    const boxes = packageStr
+    console.log(boxes)
+
+    //store count and errors
+    const countHash = {}
+    const errors = []
+
+
+    //count the occurrences
+    for (let piece of boxes) {
+      countHash[piece] = 1 + (countHash[piece] || 0)
+    }
+
+    for (let key in countHash) {
+      const lower = key.toLowerCase()
+      const upper = key.toUpperCase()
+
+      //adding count errors
+      if (countHash[lower] !== countHash[upper]) {
+        errors.push(`Error - Count Mismatch: ${lower}'s count of: ${countHash[lower] || 0} does not equal ${upper}'s count of ${countHash[upper] || 0}`)
+      }
+
+    }
+
+    const [isComplete, error] = packBoxes(boxes)
+
+    if (!isComplete && error.length > 0) {
+      errors.push(error)
+    }
+
+    if (errors.length > 0) {
+      setErrors(errors.sort((a, b) => a - b))
+    } else {
+      setSuccess(true)
+    }
+
+    return
+  }
+
+  const onChange = (e) => {
+    setPackageStr(e.target.value)
+    const cleaned = e.target.value
+      .replace(/[^wsc]/gi, "")
+
+    if (e.target.value.length !== cleaned.length) {
+      setErrors(["Invalid package record"])
+    } else {
+      setErrors([])
+    }
   }
 
   return (
     <form onSubmit={submitRecord}>
       <h1>Package Status</h1>
-      <input type="text" placeholder='Enter package record' />
+      <input
+        onChange={onChange}
+        type="text" placeholder='Enter package record' />
       <div className="stepper-container">
         {
           steps.map((step, i) => {
